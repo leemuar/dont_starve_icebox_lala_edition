7640101e3f75ef649a74d77517c96bcc57c714ea
leemuar <leemuar@gmail.com>
2017-04-09 13:23:23 +0300

    Initial version

---
+++ modinfo.lua
@@ -0,0 +1,30 @@
+--The name of the mod displayed in the 'mods' screen.
+name = "Icebox Lala Edition"
+
+--A description of the mod.
+description = "It takes longer for food to perish inside the icebox"
+
+--Who wrote this awesome mod?
+author = "leemuar"
+
+--A version number so you can ask people if they are running an old version of your mod.
+version = "1"
+
+--This lets other players know if your mod is out of date.  This typically needs to be updated every time there's a new game update.
+api_version = 6
+
+-- Compatible with both the base game and reign of giants
+dont_starve_compatible = true
+reign_of_giants_compatible = true
+shipwrecked_compatible = true
+
+--Some mods may crash or not work correctly until the game is restarted after the mod is enabled/disabled
+restart_required = false
+
+-- Set this to true to prevent _ANY_ other mods from loading while this mod is enabled.
+standalone = false
+
+--icon_atlas = "screechericon.xml"
+--icon = "screechericon.tex"
+
+forumthread = ""
